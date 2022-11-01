239cb485aad602bb37bb807d50cc7b4f5e7797f8
Dipak Thapa <dipakthapaofficial@gmail.com>
2022-11-01 09:58:53 +0545

    Adds exception class

---
+++ src/exception/demo/DemoSomething.java
@@ -0,0 +1,16 @@
+package exception.demo;
+
+
+public class DemoSomething extends ExceptionDemo {
+	
+	@Override
+	void demoSomething() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- src/exception/demo/ExceptionDemo.java
+++ src/exception/demo/ExceptionDemo.java
@@ -1,5 +1,7 @@
 package exception.demo;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -11,29 +13,105 @@ public class ExceptionDemo {
 		doSomething();
 	}
 	
-	void demoSomething() throws InputMismatchException {
+	/**
+	 * Divide two numbers
+	 * 
+	 * @param a
+	 * @param b
+	 * @return int, quotient of the division
+	 * @throws ArithmeticException
+	 */
+	
+	int divide(int a, int b) throws ArithmeticException {
+		int result = 0;
+		try {
+			result = a / b;
+		}
+		catch (Exception e) {
+			System.out.println("Do something");
+		}
+		
+		System.out.println("After division");
+		
+		return result;
+		
+	}
+	
+	boolean login(String username, String password) throws LoginException {
+		int[] a = { 1, 2, 3 };
+		
+//		try {
+			if (a.length > 4) {
+				int j = a[4];
+			}
+			
+			if (username != null) {
+				String[] something = username.split(" ");
+			} else {
+				throw new LoginException("Username is null");
+			}
+			
+			//		}
+	//		catch (NullPointerException e) {
+	//			e.printStackTrace();
+	//			System.out.println(username);
+	//			//			e = new NullPointerException();
+	//			
+	//			throw new LoginException("NUll pointer exception occurred while trying to login.");
+	//		}
+		//Check username and password is valid or not
+		//Login
+		
+		return true;
+	}
+	
+	double compute(double hours, int id) throws IOException {
+		//Get wage of the employee from network
+		URL url;
+		double result = 0;
 		//		try {
+		url = new URL("https://employee.codeinteracts.com/" + id);
+		url.openConnection();
+		
+		//wage
+		//			result = hours * wage;
+		//		}
+		//		catch ( e) {
+		//			// TODO Auto-generated catch block
+		//			e.printStackTrace();
+		//		}
+		
+		return result;
+		
+		//		
+		//
+		//		return result;
+	}
+	
+	void demoSomething() {
+		Scanner sc = new Scanner(System.in);
+		try {
 			System.out.println("Inside try block");
-			int i = new Scanner(System.in).nextInt();
+			final int i = sc.nextInt();
 			//			
 			//			System.out.println(10 / i);
 			
 			//			URL url = new URL("https://www.something.com");
-			//			//			url.openConnection();
+			//			url.openConnection();
 			//			//		
 			//			
 			//			FileReader f = new FileReader("hello.txt");
 			
 			System.out.println("Successful");
-			//		}
-		//		catch (ArithmeticException e) {
-		//			System.out.println("Inside catch block, ArithmeticException");
-		//			e.printStackTrace();
-		//		}
-		//		catch (InputMismatchException e) {
-		//			System.out.println("Inside catch block, input mismatch exception");
-		//			e.printStackTrace();
-		//		}
+		}
+		catch (ArithmeticException e) {
+			System.out.println("Inside catch block, doSomething method, ArithmeticException");
+			e.printStackTrace();
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Inside catch block, doSomething method, input mismatch exception");
+			e.printStackTrace();
+		}
 		//		catch (MalformedURLException e) {
 		//			// TODO Auto-generated catch block
 		//			System.out.println("Inside catch block, MalformedURLException");
@@ -51,19 +129,45 @@ public class ExceptionDemo {
 		//			System.out.println("Inside catch block, IOException");
 		//			e.printStackTrace();
 		//		}
+		finally {
+			System.out.println("Finally block executed.");
+			sc.close();
+		}
+		System.out.println("About to exit program");
 	}
 	
 	public static void main(String[] args) {
 		ExceptionDemo ex = new ExceptionDemo();
 		//		ex.doSomething();
 		
+		//		try {
+		//			ex.demoSomething();
+		//		}
+		//		catch (Exception e) {
+		//			System.out.println("Inside Catch block, main method");
+		//			e.printStackTrace();
+		//		}
+		
+		//		System.out.println(ex.divide(10, 0));
+		
 		try {
-			ex.demoSomething();
+			System.out.println(ex.login(null, "pass"));
+			System.out.println("enter employee");
 		}
-		catch (Exception e) {
-			System.out.println("Catch block");
+		catch (LoginException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.message);
+			e.printStackTrace();
 		}
+		
 
+		//		try {
+		//			
+		//		}
+		//		catch (ArithmeticException e) {
+		//			
+		//		}
+		
 		System.out.println("About to exit");
 		
 		System.out.println(10 + 20);
